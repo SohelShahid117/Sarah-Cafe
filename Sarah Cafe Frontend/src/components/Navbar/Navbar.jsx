b9655faf5bd9531e4f15192930471bb8b109e048
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-22 01:31:06 +0600

    navbar done-30 min,now enjoy home & cart pages

--- Sarah Cafe Frontend/src/components/Navbar/Navbar.jsx
+++ Sarah Cafe Frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(null);
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" srcset="" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu == "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu == "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" srcset="" />
@@ -18,6 +39,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" srcset="" />
           <div className="dot"></div>
         </div>
+        <button>Sign In</button>
       </div>
     </div>
   );
