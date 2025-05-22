96e211d9666aa4f318d4c2bb98227401002bf2c4
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-22 08:20:12 +0600

    sarah cafe frontend 45 min done

---
+++ Sarah Cafe Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
+          exercitationem sed explicabo earum officiis voluptatibus dolorum eius
+          reprehenderit amet ea. Vel est eligendi nostrum sunt molestias maxime
+          temporibus, ullam cum magni asperiores mollitia cumque et sint
+          aspernatur, quidem facere incidunt vitae neque magnam velit
+          consectetur ratione?
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
